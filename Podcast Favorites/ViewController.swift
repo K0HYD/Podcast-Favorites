94852f0e9ba625fc86a7ab876a20320ea84f9e50
Dale Puckett <dale.puckett@att.net>
2016-12-01 03:23:29 -0600

    Initial Commit

---
+++ Podcast Favorites/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Podcast Favorites
+//
+//  Created by Dale Puckett on 12/1/16.
+//  Copyright © 2016 k0hyd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
